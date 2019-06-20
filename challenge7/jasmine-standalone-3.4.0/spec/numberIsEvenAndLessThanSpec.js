b5c16aa039dcaa30b07857722fed54297de818ed
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-06-20 13:24:21 +0100

    refactored tests

--- challenge7/jasmine-standalone-3.4.0/spec/numberIsEvenAndLessThanSpec.js
+++ challenge7/jasmine-standalone-3.4.0/spec/numberIsEvenAndLessThanSpec.js
@@ -3,23 +3,25 @@ describe("numberIsEvenAndLessThan", function() {
     expect(typeof numberIsEvenAndLessThan(100)).toEqual("function");
   });
 
-  it("returns a function that returns true when given an even number", function() {
-    const isEven = numberIsEvenAndLessThan(100);
-    expect(isEven(10)).toEqual(true);
-  });
-
-  it("returns a function that returns false when given an odd number", function() {
-    const isEven = numberIsEvenAndLessThan(100);
-    expect(isEven(9)).toEqual(false);
-  });
-
   it("returns a function that returns true when given an even number that is less than its input", function() {
     const isEvenAndLessThan100 = numberIsEvenAndLessThan(100);
     expect(isEvenAndLessThan100(10)).toEqual(true);
   });
 
-  it("returns a function that returns false when given an even number that is greater than its input", function() {
+  it("returns a function that returns false when given an even number that is greater than or equal to its input", function() {
     const isEvenAndLessThan100 = numberIsEvenAndLessThan(100);
     expect(isEvenAndLessThan100(200)).toEqual(false);
+    expect(isEvenAndLessThan100(100)).toEqual(false);
+  });
+
+  it("returns a function that returns false when given an odd number that is less than its input", function() {
+    const isEvenAndLessThan100 = numberIsEvenAndLessThan(100);
+    expect(isEvenAndLessThan100(9)).toEqual(false);
+  });
+
+  it("returns a function that returns false when given an odd number that is greater than or equal to its input", function() {
+    const isEvenAndLessThan99 = numberIsEvenAndLessThan(99);
+    expect(isEvenAndLessThan99(201)).toEqual(false);
+    expect(isEvenAndLessThan99(99)).toEqual(false);
   });
 });
